f704acb6a554741ac0a524940a536cf6cc4f165d
Atletick <logvingleb234@gmail.com>
2026-01-19 21:10:57 +0900

    Delete novye-luydi-site directory

--- novye-luydi-site/assets/js/map.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll(".region").forEach(r => {
-  r.addEventListener("mouseenter", () => r.style.opacity = "0.7");
-  r.addEventListener("mouseleave", () => r.style.opacity = "1");
-});
